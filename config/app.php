4dcdfe4de9fec4d0d4936790a6d0c308414914c9
andela-cvundi <christophervundi@gmail.com>
2016-03-14 20:03:32 +0300

    [Feature #115584095] Update config/app.php by adding cloudinary provider and alias

--- config/app.php
+++ config/app.php
@@ -147,6 +147,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        JD\Cloudder\CloudderServiceProvider::class,
+
 
         /*
          * Application Service Providers...
@@ -201,6 +203,8 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+        'Cloudder'  => JD\Cloudder\Facades\Cloudder::class,
+
 
     ],
 
