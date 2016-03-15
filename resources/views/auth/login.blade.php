0ec0e1820c7e9fdb49fc2ba149e4aee142ba5434
andela-cvundi <christophervundi@gmail.com>
2016-03-15 20:20:07 +0300

    [Feature #115584095] Add facebook, github and twitter buttons for sign up

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -58,6 +58,22 @@
                             </div>
                         </div>
                     </form>
+                    <hr>
+                    <div class="social-auth-links text-center">
+                        <p>- OR -</p>
+                        <a href="http://ocademy.dev/auth/facebook" class="btn btn-block btn-facebook">
+                            <i class="fa fa-facebook"></i>
+                                Sign in with Facebook
+                        </a>
+                        <a href="http://ocademy.dev/auth/twitter" class="btn btn-block btn-twitter">
+                            <i class="fa fa-twitter"></i>
+                                Sign in with Twitter
+                        </a>
+                        <a href="http://ocademy.dev/auth/github" class="btn btn-block btn-github">
+                            <i class="fa fa-github-alt"></i>
+                                Sign in with Github
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
