c0549ee05589abf1cd05a1a07d7e8cc6ba3036eb
andela-cvundi <christophervundi@gmail.com>
2016-03-15 22:55:56 +0300

    [Feature #115584537] Define relationships that exist within a tutorial

--- app/Tutorial.php
+++ app/Tutorial.php
@@ -2,9 +2,20 @@
 
 namespace Ocademy;
 
+use Ocademy\User;
+use Ocademy\Category;
 use Illuminate\Database\Eloquent\Model;
 
 class Tutorial extends Model
 {
-    //
+    protected $fillable = ['title', 'description', 'url', 'user_id', 'category_id'];
+
+    public function category()
+    {
+        return $this->belongsTo('Techademia\Category');
+    }
+    public function user()
+    {
+        return $this->belongsTo('Techademia\User');
+    }
 }
