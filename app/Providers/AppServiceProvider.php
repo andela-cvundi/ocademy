eb6449e1e6d59d7915b9817f9c7fc8896b5c06a5
andela-cvundi <christophervundi@gmail.com>
2016-03-15 22:54:30 +0300

    [Feature #115584537] Validate youtube url

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Validator::extend('youtube', function ($attribute, $value, $parameters, $validator) {
+            return [$value] == preg_grep("%^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/watch\?v=([^&]+)%", [$value]);
+        }, 'Invalid youtube video url');
     }
 
     /**
