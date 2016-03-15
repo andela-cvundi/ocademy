56e42b9530f2bd04a3eb65cf0e08fac2abe7fa83
andela-cvundi <christophervundi@gmail.com>
2016-03-15 20:17:51 +0300

    [Feature #115584095] Make email and password field nullable

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,8 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('username')->unique()->nullable();
-            $table->string('email')->unique();
-            $table->string('password', 60);
+            $table->string('email')->unique()->nullable();
+            $table->string('password', 60)->nullable();
             $table->string('provider')->nullable();
             $table->string('provider_id')->unique()->nullable();
             $table->string('avatar')->nullable();
