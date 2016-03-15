a6adcf7822bf61dfe4c62882cb441346f96d12b5
andela-cvundi <christophervundi@gmail.com>
2016-03-15 20:15:30 +0300

    [Feature #115584095] Add outh methods to work with different providers

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -2,8 +2,10 @@
 
 namespace Ocademy\Http\Controllers\Auth;
 
+use Auth;
 use Ocademy\User;
 use Validator;
+use Socialite;
 use Ocademy\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ThrottlesLogins;
 use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
@@ -67,6 +69,57 @@ class AuthController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
+        ]);
+    }
+
+    /**
+     * Redirect the user to the provider authentication page.
+     *
+     * @return Response
+     */
+    public function redirectToProvider($provider)
+    {
+        return Socialite::driver($provider)->redirect();
+    }
+
+    /**
+     * Obtain the user information from provider.
+     *
+     * @return Response
+     */
+    public function handleProviderCallback($provider)
+    {
+        try {
+            $user = Socialite::driver($provider)->user();
+        } catch (Exception $e) {
+            return redirect('auth/'.$provider);
+        }
+
+        $authUser = $this->findOrCreateUser($user, $provider);
+
+        Auth::login($authUser, true);
+
+        return redirect($this->redirectTo);
+    }
+
+    /**
+     * Find user based on Oauth details, or create one if none is found.
+     */
+    public function findOrCreateUser($user, $provider)
+    {
+        $authUser = User::where('provider_id', $user->id)->first();
+
+        if ($authUser) {
+            return $authUser;
+        }
+
+        return User::create([
+            'name'     => $user->name,
+            'username' => $user->nickname,
+            'email'    => $user->email,
+            'provider' => $provider,
+            'provider_id'      => $user->id,
+            'avatar'   => isset($user->avatar_original) ? $user->avatar_original : $user->avatar,
         ]);
     }
 }
