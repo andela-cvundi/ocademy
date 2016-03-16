9cfdf6125586623f39bc03b91d2034b698094b0c
andela-cvundi <christophervundi@gmail.com>
2016-03-16 22:43:28 +0300

    [Feature #115584537] DEfine relationships in the category model

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $table = 'categories';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['title'];
+    public function tutorials()
+    {
+        return $this->hasMany('Ocademy\Tutorial');
+    }
 }
