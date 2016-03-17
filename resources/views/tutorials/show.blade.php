e30354ca4346667ac10b7aca69568dbfb4769418
andela-cvundi <christophervundi@gmail.com>
2016-03-17 11:47:09 +0300

    [Feature #115584537] Add delete and edit buttons in the show view

--- resources/views/tutorials/show.blade.php
+++ resources/views/tutorials/show.blade.php
@@ -8,10 +8,23 @@
     </div>
 
 
-    <div class="col-md-9">
+    <div class="row">
+        <div class="col-md-9">
         <div id="tutorial-description" class="well">
             <p>{{ $tutorial->description }}</p>
         </div>
     </div>
+    <div class="col-md-3">
+    @if (Auth::check())
+    <a href="{{ url('tutorials/'.$tutorial->id.'/edit') }}" class="btn btn-info">Edit</a>
+    <form action="{{ url('tutorials/'.$tutorial->id) }}" method="POST">
+        {{ method_field('DELETE') }}
+        <input type="hidden" id="token" name="_token" value="{{ csrf_token() }}">
+        <button type="submit" class="btn btn-danger">Delete</a>
+    </form>
+    @endif
+    </div>
+    </div>
+
 </div>
 @endsection
