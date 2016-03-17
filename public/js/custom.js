703e6d8c1f3b0bd8bd38c68b90a3ef72b10cf750
andela-cvundi <christophervundi@gmail.com>
2016-03-17 22:48:59 +0300

    [Feature #115584679] Add ajax for appending new comments

--- public/js/custom.js
+++ public/js/custom.js
@@ -6,10 +6,10 @@ $(document).ready(function() {
     });
     $('#like').on("click", function ()
     {
-        var video_id = $(this).attr('data-video-id')
+        var tut_id = $(this).attr('tutorial-id')
         $x = $.ajax({
             method: 'POST',
-            url: '/tutorials/' + video_id + '/like'
+            url: '/tutorials/' + tut_id + '/like'
         });
         var num = Number($('#count').text());
 
@@ -22,5 +22,40 @@ $(document).ready(function() {
                 $('#count').text(' ' + --num);
             }
         });
+    });
+
+    $('#createComment').on("click", function (e)
+    {
+        var comment = $('#comment').val().trim();
+        var tut_id = $(this).attr('tutorial-id');
+
+        if (comment.length !== 0) {
+            addNewComment = $.ajax({
+                type : 'POST',
+                url: '/tutorials/comment',
+                data: {
+                    comment:  comment,
+                    video_id: tut_id
+                }
+            });
+            addNewComment.done(function (response) {
+                $('#comment').val('');
+                var newComment = '';
+                newComment = '<li class="media">';
+                newComment += '<a class="media-left" href="#">';
+                newComment += '<img class="media-object img-circle img-thumbnail" src='+ response.user.avatar +' style="width:100px;">';
+                newComment += '</a>';
+                newComment += '<div class="media-body">';
+                newComment += '<div class="well">'
+                newComment += '<h4 class="media-heading">'+ response.user.name +'</h4>';
+                newComment += '<h6 class="pull-right">1 second ago </h6>';
+                newComment += '<p class="media-comment">' + comment + '</p>';
+                newComment += '</div>';
+                newComment += '</div>';
+                newComment += '</li>';
+
+                $('.media-list').append(newComment);
+            });
+        }
     });
 });
