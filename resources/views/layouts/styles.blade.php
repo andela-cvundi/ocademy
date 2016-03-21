f94c3fa10a878da6b8d367a17d50a20850a4c702
andela-cvundi <christophervundi@gmail.com>
2016-03-21 21:59:14 +0300

    [Feature #115584309] Add styling for the dashboard

--- resources/views/layouts/styles.blade.php
+++ resources/views/layouts/styles.blade.php
@@ -4,4 +4,5 @@
 <link rel="stylesheet" href="/css/bootstrap.min.css">
 <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.2.0/styles/default.min.css">
 <link rel="stylesheet" href="/css/styles.css">
+<link rel="stylesheet" href="/css/dashboard.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
