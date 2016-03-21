e30d66221cf012dae1e7e4e76a4edc74202b96be
andela-cvundi <christophervundi@gmail.com>
2016-03-22 00:20:47 +0300

    [Fix] Rectify oauth urls

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -51,8 +51,8 @@
                     <p style="text-align: center">-OR-</p>
                     <div class="center-button">
                         <a href="{{ url('/auth/facebook') }}" class="btn btn-facebook"><i class="fa fa-facebook"></i> Facebook</a>
-                        <a href="url('/auth/twitter') }}" class="btn btn-twitter" class="btn btn-twitter"><i class="fa fa-twitter"></i> Twitter</a>
-                        <a href="url('/auth/github') }}" class="btn btn-github"><i class="fa fa-github"></i> Github</a>
+                        <a href="{{ url('/auth/twitter') }}" class="btn btn-twitter" class="btn btn-twitter"><i class="fa fa-twitter"></i> Twitter</a>
+                        <a href="{{ url('/auth/github') }}" class="btn btn-github"><i class="fa fa-github"></i> Github</a>
                     </div>
                 </div>
                 <div class="registration">
