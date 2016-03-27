93f6a91b17a990de5b68960947dbe4cc8465d4eb
andela-cvundi <christophervundi@gmail.com>
2016-03-27 15:56:47 +0300

    [Fix] Add sweet alert promp before deleting tutorial
    [Fix] Add sweet alert css

--- public/js/custom.js
+++ public/js/custom.js
@@ -4,8 +4,7 @@ $(document).ready(function() {
             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
         }
     });
-    $('#like').on("click", function ()
-    {
+    $('#like').on("click", function() {
         var tut_id = $(this).attr('tutorial-id')
         $x = $.ajax({
             method: 'POST',
@@ -13,38 +12,37 @@ $(document).ready(function() {
         });
         var num = Number($('#count').text());
 
-        $x.done(function (response) {
+        $x.done(function(response) {
             console.log(response.message);
 
             if (response.message == 'liked') {
                 $('#count').text(' ' + ++num);
-            } else if(response.message == 'unliked') {
+            } else if (response.message == 'unliked') {
                 $('#count').text(' ' + --num);
             }
         });
     });
 
-    $('#createComment').on("click", function (e)
-    {
+    $('#createComment').on("click", function(e) {
         var comment = $('#comment').val().trim();
         var tut_id = $(this).attr('tutorial-id');
 
         if (comment.length !== 0) {
             addNewComment = $.ajax({
-                type : 'POST',
+                type: 'POST',
                 url: '/tutorials/comment',
                 data: {
-                    comment:  comment,
+                    comment: comment,
                     video_id: tut_id
                 }
             });
-            addNewComment.done(function (response) {
+            addNewComment.done(function(response) {
                 $('#comment').val('');
                 var newComment = '';
 
                 newComment = '<li class="comment">';
                 newComment += '<article>';
-                newComment += '<img class="avatar" src='+ response.user.avatar +' alt="userAvatar" width="50px" height="50px">';
+                newComment += '<img class="avatar" src=' + response.user.avatar + ' alt="userAvatar" width="50px" height="50px">';
                 newComment += '<div class="comment-meta">';
                 newComment += '<h6 class="author">' + response.user.name + ', 1 second ago </h6>';
                 newComment += '</div>';
@@ -63,14 +61,49 @@ $(document).ready(function() {
             });
         }
     });
+    $('.delete-tutorial').on("click", function(event) {
+        event.preventDefault();
+        var this_ = $(this);
 
-    $('.delete-tutorial').on('click', function (e)
-    {
-        e.preventDefault();
-        $x = $.ajax({
-            method: 'DELETE',
-            url: $(this).attr('href')
-        });
-        window.location.href = '/profile/tutorials';
+        swal({
+                title: "Are you sure?",
+                text: "Your will not be able to recover this Video",
+                type: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#DD6B55",
+                confirmButtonClass: "btn-danger",
+                confirmButtonText: "Yes, delete it!",
+                closeOnConfirm: false,
+                showLoaderOnConfirm: true
+            },
+            function() {
+                $deleteVideo = $.ajax({
+                    method: 'DELETE',
+                    'url': this_.attr('href')
+                });
+
+                $deleteVideo.done(function(response) {
+                    if (response.message === "deleted") {
+                        window.location = '/profile/tutorials';
+                    } else {
+                        swal({
+                            title: "Error",
+                            text: "Error deleting video",
+                            type: "error",
+                            showCancelButton: true
+                        });
+                    }
+                });
+
+                //network error or any other reason
+                $deleteVideo.fail(function (response) {
+                     swal({
+                            title: "Error",
+                            text: "Error deleting video",
+                            type: "error",
+                            showCancelButton: true
+                        });
+                });
+            });
     });
 });
--- resources/views/layouts/js.blade.php
+++ resources/views/layouts/js.blade.php
@@ -1,3 +1,4 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 <script src="{{ url('/js/custom.js') }}"></script>
+<script src="{{ URL::asset('bower/sweetalert/dist/sweetalert.min.js', env('APP_ENV') == 'production') }}"></script>
